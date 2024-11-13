993580c3a130ff202dcc145edcbd09463c5c60f9
Yasaman Kashefbahrami <ykbahrami@gmail.com>
2024-11-13 15:02:21 +0100

    dataset read and write

--- HD5Write/Program.cs
+++ HD5Write/Program.cs
@@ -1,8 +1,11 @@
 ﻿// Example from https://www.nuget.org/packages/HDF5-CSharp
 // Install HDF5-CSharp from NuGet Package Manager or 'dotnet add package HDF5-CSharp --version 1.19.0'
 // This code creates a HDF5 file and writes an object to it
-
 using HDF5CSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace HD5Write
 {
@@ -16,37 +19,37 @@ namespace HD5Write
         public string TestString { get; set; }
         public double[] TestReals { get; set; }
         public double[] TestImaginaries { get; set; }
+    }
 
-        class Program
+    class Program
+    {
+        static void Main(string[] args)
         {
-            static void Main(string[] args)
+            // Create an instance of TestClass
+            var testClass = new TestClass
             {
-                // Create an instance of TestClasss
-                var testClass = new TestClass
-                {
-                    TestInteger = 2,
-                    TestDouble = 1.1,
-                    TestBoolean = true,
-                    TestString = "test string",
-                    TestDoubles = new double[] { 1.1, 1.2, -1.1, -1.2 },
-                    TestStrings = new string[] { "one", "two", "three", "four" },
-                    TestReals = new double[] { 5.6222, 9.575753, 10 },
-                    TestImaginaries = new double[] { -13242, 0.6243, -2.95353 }
-                };
+                TestInteger = 2,
+                TestDouble = 1.1,
+                TestBoolean = true,
+                TestString = "test string",
+                TestDoubles = new double[] { 1.1, 1.2, -1.1, -1.2 },
+                TestStrings = new string[] { "one", "two", "three", "four" },
+                TestReals = new double[] { 5.6222, 9.575753, 10 },
+                TestImaginaries = new double[] { -13242, 0.6243, -2.95353 }
+            };
 
+            // Create a HDF5 file
+            string myPath = "C:\\Users\\stude\\Desktop\\Learning\\HD5Write\\";
+            string filename = Path.Combine(myPath, "anothertestFile.H5");
+            long fileId = Hdf5.CreateFile(filename);
 
-                // Create a HDF5 file
-                string myPath = "C:\\Users\\stude\\Desktop\\Learning\\HD5Write\\";
-                long fileId = Hdf5.CreateFile(myPath + "testFile.H5");
+            // Write the object to the file
+            Hdf5.WriteObject(fileId, testClass, "testObject");
 
-                // Write the object to the file
-                Hdf5.WriteObject(fileId, testClass, "testObject");
+            // Close the file
+            Hdf5.CloseFile(fileId);
 
-                // Close the file
-                Hdf5.CloseFile(fileId);
-
-                Console.WriteLine("File Created!");
-            }
+            Console.WriteLine("File Created!");
         }
     }
 }
---
+++ HDF5Dataset/Program.cs
@@ -0,0 +1,87 @@
+﻿// Example from https://www.nuget.org/packages/HDF5-CSharp
+// Install HDF5-CSharp: 'dotnet add package HDF5-CSharp --version 1.19.0'
+// This code creates a HDF5 file and writes an object to it
+
+using HDF5CSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace HD5Dataset
+{
+    class Program
+    {
+
+        /// <summary>
+        /// Create a matrix and fill it with numbers
+        /// </summary>
+        /// <param name="offset"></param>
+        /// <returns>The matrix</returns>
+        private static double[,] createDataset(int offset = 0)
+        {
+            var dset = new double[10, 5];
+            for (var i = 0; i < 10; i++)
+                for (var j = 0; j < 5; j++)
+                {
+                    double x = i + j * 5 + offset;
+                    dset[i, j] = (j == 0) ? x : x / 10;
+                }
+            return dset;
+        }
+
+        /// <summary>
+        /// Print the matrix to the console
+        /// </summary>
+        /// <param name="matrix">The matrix to print</param>
+        private static void PrintMatrix(double[,] matrix)
+        {
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.Write(matrix[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+        static void Main(string[] args)
+        {
+
+            // Create a list of matrices
+            var dsets = new List<double[,]>
+        {
+            createDataset(),
+            createDataset(10),
+            createDataset(20)
+        };
+
+            string myPath = "C:\\Users\\stude\\Desktop\\Learning\\HDF5Dataset";
+            string filename = Path.Combine(myPath, "testChunks.H5");
+            long fileId = Hdf5.CreateFile(filename);
+
+            // Create a dataset and append two more datasets to it
+            using (var chunkedDset = new ChunkedDataset<double>("/test", fileId, dsets.First()))
+            {
+                foreach (var ds in dsets.Skip(1))
+                    chunkedDset.AppendDataset(ds);
+            }
+
+            // Read rows 9 to 22 of the dataset
+            ulong begIndex = 8;
+            ulong endIndex = 21;
+            var dset = Hdf5.ReadDataset<double>(fileId, "/test", begIndex, endIndex);
+            Hdf5.CloseFile(fileId);
+
+            // Print the dataset
+            PrintMatrix(dset);
+
+            Console.WriteLine("Chunked dataset created and subset read!");
+        }
+    }
+}
+
+
